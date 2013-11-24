8f57259381ada1848a4a36642326e4a4ddaec028
iwasakims <iwasakims@example.com>
2013-11-24 14:15:32 -0800

    initializing SpanReceiverHost in AbstraceService

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/service/AbstractService.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/service/AbstractService.java
@@ -30,6 +30,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.classification.InterfaceAudience.Public;
 import org.apache.hadoop.classification.InterfaceStability.Evolving;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.tracing.SpanReceiverHost;
 
 import com.google.common.annotations.VisibleForTesting;
 
@@ -172,6 +173,7 @@ public abstract class AbstractService implements Service {
           throw ServiceStateException.convert(e);
         }
       }
+      SpanReceiverHost.init(conf);
     }
   }
 
